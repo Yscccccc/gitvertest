7cdf4a09fa1499eed37956c47f6dc30c6878b0ec
CSY <575570818@qq.com>
2018-07-25 14:30:08 +0800

    Initial commit

---
+++ src/main/java/com/ysc/gitvertest/GitvertestApplication.java
@@ -0,0 +1,12 @@
+package com.ysc.gitvertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitvertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitvertestApplication.class, args);
+    }
+}
